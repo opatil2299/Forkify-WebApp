ebb11fff87a43c03b08467d9daa1f46e18d93922
Omkar <opatil2299@gmail.com>
2022-01-21 14:05:54 +0530

    Third commit

--- src/js/config.js
+++ src/js/config.js
@@ -3,4 +3,3 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '1072f25a-24ec-4db7-b19c-c2f4236f7637';
 export const MODAL_CLOSE_SEC = 2.5;
-console.log('Welcome baby');
