a8d6b9a4f29a0bafbb624f284cf69199395335ec
Omkar <opatil2299@gmail.com>
2022-01-21 13:26:03 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1072f25a-24ec-4db7-b19c-c2f4236f7637';
+export const MODAL_CLOSE_SEC = 2.5;
